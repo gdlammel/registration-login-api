65f17ae7f4f3c74d2741fca5dc908f5deb279d88
Guilherme Duarte <gdlammel04@gmail.com>
2023-03-21 19:13:28 -0300

    test(domain): add user entity test

---
+++ src/domain/entities/user.test.ts
@@ -0,0 +1,43 @@
+import { describe, it, expect } from "vitest";
+import { User, IUserProps } from "@/domain/entities/user";
+
+describe("Create user entity", () => {
+	it("should be able to create new user", () => {
+		const user: IUserProps = {
+			id: "1",
+			name: "Teste",
+			password: "123",
+			email: "teste@gmail.com",
+			phoneNumber: 12345678,
+		};
+		expect(User.create(user)).toBeInstanceOf(User);
+	});
+
+	it("should not be able to create new user by passing an invalid email", () => {
+		const user: IUserProps = {
+			id: "1",
+			name: "Teste",
+			password: "123",
+			email: "fewuhwhweu",
+			phoneNumber: 12345678,
+		};
+
+		expect(() => {
+			User.create(user);
+		}).toThrowError();
+	});
+
+	it("should not be able to create new user by passing an invalid phone number", () => {
+		const user: IUserProps = {
+			id: "1",
+			name: "teste",
+			password: "123",
+			email: "teste@gmail.com",
+			phoneNumber: 54385,
+		};
+
+		expect(() => {
+			User.create(user);
+		}).toThrowError();
+	});
+});
