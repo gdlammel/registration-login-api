abf3061e78e54599a0348a6dfea46da35302b610
Guilherme Duarte <gdlammel04@gmail.com>
2023-03-24 22:46:41 -0300

    feat(application) create forgot password use case

---
+++ src/application/use-cases/forgot-password/forgot-password.ts
@@ -0,0 +1,41 @@
+import {
+	IEmailProvider,
+	ITokenManagerProvider,
+} from "@/application/contracts/providers";
+import { IUserRepository } from "@/application/contracts/repositories";
+import { UseCase } from "@/domain";
+import { User, UserDomainError } from "@/domain/entities";
+import { EmailNotFoundError } from "@/application/use-cases/forgot-password/errors";
+
+export interface ForgotPasswordInputDTO {
+	email: string;
+}
+
+export class ForgotPasswordUseCase
+	implements UseCase<ForgotPasswordInputDTO, boolean | EmailNotFoundError>
+{
+	constructor(
+		private userRepository: IUserRepository,
+		private tokenManagerService: ITokenManagerProvider,
+		private emailProvider: IEmailProvider
+	) {}
+	async execute({
+		email,
+	}: ForgotPasswordInputDTO): Promise<boolean | EmailNotFoundError> {
+		const emailExists = await this.userRepository.findUserByEmail(email);
+		if (!emailExists) {
+			return new EmailNotFoundError();
+		}
+		const createdUserOrError = User.create(emailExists);
+		if (createdUserOrError instanceof Error) {
+			return new UserDomainError();
+		}
+
+		const token = this.tokenManagerService.generate(createdUserOrError);
+		const result = await this.emailProvider.sendEmail(
+			createdUserOrError,
+			token
+		);
+		return result;
+	}
+}
