7e8dddae53358401af30748d7b6832ca333baaac
Guilherme Duarte <gdlammel04@gmail.com>
2023-03-25 04:26:54 -0300

    feat(application) update user repository contract to add findUserById and UpdatePassword methods

--- src/application/contracts/repositories/user.ts
+++ src/application/contracts/repositories/user.ts
@@ -3,4 +3,6 @@ import { User } from "@/domain/entities";
 export interface IUserRepository {
 	save(user: User): Promise<User>;
 	findUserByEmail(email: string): Promise<User | null>;
+	findUserByID(id: string): Promise<User | null>;
+	updatePassword(user: User, newPassword: string): Promise<boolean>;
 }
