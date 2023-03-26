806019a31474fba3614b4133fe73b09094f24c33
Guilherme Duarte <gdlammel04@gmail.com>
2023-03-25 23:41:34 -0300

    feat(main): add appRouter to app config

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,6 +1,9 @@
 import express from "express";
+import { appRouter } from "@/main/config";
 
 const app = express();
 app.use(express.json());
 
-export default app;
+app.use("/api", appRouter);
+
+export { app };
