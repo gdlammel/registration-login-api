3a11ed4d4308eabd28d748443b9a89c7d1946a0f
Guilherme Duarte <gdlammel04@gmail.com>
2023-03-26 04:09:03 -0300

    feat(infra): add create user controller code logic

--- src/infra/controllers/create-user.ts
+++ src/infra/controllers/create-user.ts
@@ -1,6 +1,41 @@
 import { Request, Response } from "express";
 import { CreateUserUseCase } from "@/application/use-cases/create-user";
-export class CreateUserController {
+import { Controller, ResponseData } from "@/infra/controllers/common";
+import {
+	EmailAlreadyRegisteredError,
+	InternalError,
+} from "@/application/use-cases/create-user/errors";
+import { UserDomainError } from "@/domain/entities";
+
+interface HttpCreateUserParams {
+	name: string;
+	password: string;
+	email: string;
+	phoneNumber: number;
+}
+export class CreateUserController implements Controller {
 	constructor(private createUserUseCase: CreateUserUseCase) {}
-	async handle(request: Request, response: Response) {}
+	async handle(request: Request, response: Response): Promise<any> {
+		const { name, password, email, phoneNumber }: HttpCreateUserParams =
+			request.body;
+		const result = await this.createUserUseCase.execute({
+			name,
+			password,
+			email,
+			phoneNumber,
+		});
+
+		if (
+			result instanceof EmailAlreadyRegisteredError ||
+			result instanceof UserDomainError
+		) {
+			return response.json(ResponseData.badRequest(result));
+		} else if (result instanceof InternalError) {
+			return response.json(ResponseData.internalError(result));
+		} else {
+			return response.json(
+				ResponseData.created("User created with success!")
+			);
+		}
+	}
 }
