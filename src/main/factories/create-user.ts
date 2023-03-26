bee20549c4f4f0266f2367efad2a548751b7a2d9
Guilherme Duarte <gdlammel04@gmail.com>
2023-03-25 23:48:01 -0300

    feat(main): create create-user factory

---
+++ src/main/factories/create-user.ts
@@ -0,0 +1,21 @@
+import { CreateUserUseCase } from "@/application/use-cases/create-user";
+import { CreateUserController } from "@/infra/controllers";
+import {
+	InMemoryGenerateIDProvider,
+	InMemoryHashPasswordProvider,
+} from "@/infra/providers";
+import { InMemoryUserRepository } from "@/infra/repositories";
+
+const userRepository = new InMemoryUserRepository([]);
+const generateIDProvider = new InMemoryGenerateIDProvider();
+const hashPasswordProvider = new InMemoryHashPasswordProvider();
+
+const useCase = new CreateUserUseCase(
+	userRepository,
+	generateIDProvider,
+	hashPasswordProvider
+);
+
+const createUserController = new CreateUserController(useCase);
+
+export { createUserController };
