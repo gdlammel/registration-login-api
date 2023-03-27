a5fae67509f683551c06f2bf24da17125d433293
Guilherme Duarte <gdlammel04@gmail.com>
2023-03-26 21:48:25 -0300

    refactor(infra): create a variable before sending the response to be able to add the status

--- src/infra/controllers/create-user.ts
+++ src/infra/controllers/create-user.ts
@@ -1,10 +1,8 @@
 import { Request, Response } from "express";
 import { CreateUserUseCase } from "@/application/use-cases/create-user";
 import { Controller, ResponseData } from "@/infra/controllers/common";
-import {
-	EmailAlreadyRegisteredError,
-	InternalError,
-} from "@/application/use-cases/create-user/errors";
+import { EmailAlreadyRegisteredError } from "@/application/use-cases/create-user/errors";
+import { InternalError } from "@/application/use-cases/common/errors";
 import { UserDomainError } from "@/domain/entities";
 
 interface HttpCreateUserParams {
@@ -24,18 +22,22 @@ export class CreateUserController implements Controller {
 			email,
 			phoneNumber,
 		});
-
 		if (
 			result instanceof EmailAlreadyRegisteredError ||
 			result instanceof UserDomainError
 		) {
-			return response.json(ResponseData.badRequest(result));
-		} else if (result instanceof InternalError) {
-			return response.json(ResponseData.internalError(result));
-		} else {
-			return response.json(
-				ResponseData.created("User created with success!")
+			const responseData = ResponseData.badRequest(
+				"Error creating new user"
 			);
+			return response.status(responseData.statusCode).json(responseData);
+		} else if (result instanceof InternalError) {
+			const responseData = ResponseData.internalError("Internal error");
+			return response.status(responseData.statusCode).json(responseData);
+		} else {
+			const responseData = ResponseData.created(
+				"User created with success!"
+			);
+			return response.status(responseData.statusCode).json(responseData);
 		}
 	}
 }
