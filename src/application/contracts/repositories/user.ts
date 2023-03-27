b70cff6ca9c5ccb3fb2cc0b221a98e59b5dc0e14
Guilherme Duarte <gdlammel04@gmail.com>
2023-03-26 21:34:35 -0300

    refactor(application): change save method return to a boolean

--- src/application/contracts/repositories/user.ts
+++ src/application/contracts/repositories/user.ts
@@ -1,7 +1,7 @@
 import { User } from "@/domain/entities";
 
 export interface IUserRepository {
-	save(user: User): Promise<User>;
+	save(user: User): Promise<boolean>;
 	findUserByEmail(email: string): Promise<User | null>;
 	findUserByID(id: string): Promise<User | null>;
 	updatePassword(user: User, newPassword: string): Promise<boolean>;
