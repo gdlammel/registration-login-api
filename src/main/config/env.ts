eca8e218b2968e470b8ff46787898f1b48abd4aa
Guilherme Duarte <gdlammel04@gmail.com>
2023-03-27 22:16:48 -0300

    chore: add jwt config to .env file

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -2,4 +2,6 @@ import * as dotenv from "dotenv";
 dotenv.config();
 export const env = {
 	port: process.env.PORT,
+	secret: process.env.JWT_SECRET as string,
+	expiresIn: process.env.JWT_EXPIRES_IN,
 };
