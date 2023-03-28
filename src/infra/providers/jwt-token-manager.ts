f07c28e70dab2d66c20bbadfa5a116d868c20015
Guilherme Duarte <gdlammel04@gmail.com>
2023-03-27 22:25:41 -0300

    feat(infra): create jwt token manager provider implementation

---
+++ src/infra/providers/jwt-token-manager.ts
@@ -0,0 +1,18 @@
+import { ITokenManagerProvider } from "@/application/contracts/providers";
+import { env } from "@/main/config";
+import { User } from "@/domain/entities";
+import { sign } from "jsonwebtoken";
+
+export class JwtTokenMangerProvider implements ITokenManagerProvider {
+	generate(data: User): string {
+		const token = sign({ id: data.id }, env.secret, {
+			subject: data.id,
+			expiresIn: env.expiresIn,
+		});
+
+		return token;
+	}
+	verify(token: string): Object {
+		return "Method not implemented";
+	}
+}
