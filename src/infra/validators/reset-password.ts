f01ca7bfa5e8ae5f65cf27097c3c5e4a866434eb
gdlammel <gdlammel04@gmail.com>
2023-03-28 23:37:48 -0300

    feat(infra): create reset password validator

---
+++ src/infra/validators/reset-password.ts
@@ -0,0 +1,38 @@
+import { Request, Response, NextFunction } from "express";
+import { z, ZodError } from "zod";
+import { ResponseData } from "@/infra/controllers/common";
+
+const resetPasswordInputSchema = z.object({
+	id: z.string().nonempty({ message: "Id is required" }),
+	newPassword: z
+		.string()
+		.nonempty({ message: "Pasword is required" })
+		.min(8, { message: "Password must be at least 8 characters" }),
+});
+
+export type ResetPasswordOutputData = z.output<typeof resetPasswordInputSchema>;
+
+export class validateResetPasswordInput {
+	static validate(request: Request, response: Response, next: NextFunction) {
+		const { id, newPassword } = request.body;
+		try {
+			const validatedData = resetPasswordInputSchema.parse({
+				id,
+				newPassword,
+			});
+			request.body = validatedData;
+			return next();
+		} catch (error) {
+			if (error instanceof ZodError) {
+				const responseData = ResponseData.badRequest(
+					error.issues[0].message
+				);
+				return response
+					.status(responseData.statusCode)
+					.json(responseData);
+			}
+			const responseData = ResponseData.internalError("Internal error");
+			return response.status(responseData.statusCode).json(responseData);
+		}
+	}
+}
