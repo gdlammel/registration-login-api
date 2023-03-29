601c066887298ed75c4f70241d37eb73eccb28ed
gdlammel <gdlammel04@gmail.com>
2023-03-28 23:44:21 -0300

    feat(main): create reset password endpoint

--- src/main/routes/user.ts
+++ src/main/routes/user.ts
@@ -1,14 +1,27 @@
 import { Router } from "express";
-import { CreateUserFactory } from "@/main/factories";
-import { validateCreateUserInput } from "@/infra/validators";
+import { CreateUserFactory, ResetPasswordFactory } from "@/main/factories";
+import {
+	validateCreateUserInput,
+	validateResetPasswordInput,
+} from "@/infra/validators";
+import { EnsureAuthenticationMiddleware } from "@/infra/middlewares";
 
 const userRoutes = Router();
 
 const createUserController = CreateUserFactory.create();
+const resetPasswordController = ResetPasswordFactory.create();
+
 userRoutes.post(
 	"/",
 	validateCreateUserInput.validate,
 	createUserController.handle.bind(createUserController)
+);
+
+userRoutes.patch(
+	"/reset-password",
+	EnsureAuthenticationMiddleware.verify,
+	validateResetPasswordInput.validate,
+	resetPasswordController.handle.bind(resetPasswordController)
 );
 
 export { userRoutes };
