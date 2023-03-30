d66debc0801f571671ee6eb4ebdce07623b3051a
gdlammel <gdlammel04@gmail.com>
2023-03-29 22:56:31 -0300

    feat(main): create forgot password endpoint

--- src/main/routes/user.ts
+++ src/main/routes/user.ts
@@ -1,8 +1,13 @@
 import { Router } from "express";
-import { CreateUserFactory, ResetPasswordFactory } from "@/main/factories";
+import {
+	CreateUserFactory,
+	ForgotPasswordFactory,
+	ResetPasswordFactory,
+} from "@/main/factories";
 import {
 	validateCreateUserInput,
 	validateResetPasswordInput,
+	validateForgotPasswordInput,
 } from "@/infra/validators";
 import { EnsureAuthenticationMiddleware } from "@/infra/middlewares";
 
@@ -10,11 +15,18 @@ const userRoutes = Router();
 
 const createUserController = CreateUserFactory.create();
 const resetPasswordController = ResetPasswordFactory.create();
+const forgotPasswordController = ForgotPasswordFactory.create();
 
 userRoutes.post(
 	"/",
 	validateCreateUserInput.validate,
 	createUserController.handle.bind(createUserController)
+);
+
+userRoutes.post(
+	"/forgot-password",
+	validateForgotPasswordInput.validate,
+	forgotPasswordController.handle.bind(forgotPasswordController)
 );
 
 userRoutes.patch(
