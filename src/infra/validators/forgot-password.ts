1db5097519ea18ba69f19a9189bf2d9b285b82ba
gdlammel <gdlammel04@gmail.com>
2023-03-29 22:55:16 -0300

    feat(infra): create forgot password validator

---
+++ src/infra/validators/forgot-password.ts
@@ -0,0 +1,33 @@
+import { Request, Response, NextFunction } from "express";
+import { z, ZodError } from "zod";
+import { ResponseData } from "@/infra/controllers/common";
+
+const forgotPasswordSchema = z.object({
+	email: z
+		.string()
+		.email({ message: "Email invalid format" })
+		.nonempty({ message: "Email is required" }),
+});
+
+export type forgotPasswordOutputData = z.output<typeof forgotPasswordSchema>;
+
+export class validateForgotPasswordInput {
+	static validate(request: Request, response: Response, next: NextFunction) {
+		const { email } = request.body;
+		try {
+			forgotPasswordSchema.parse({ email });
+			return next();
+		} catch (error) {
+			if (error instanceof ZodError) {
+				const responseData = ResponseData.badRequest(
+					error.issues[0].message
+				);
+				return response
+					.status(responseData.statusCode)
+					.json(responseData);
+			}
+			const responseData = ResponseData.internalError("Internal error");
+			return response.status(responseData.statusCode).json(responseData);
+		}
+	}
+}
