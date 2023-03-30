04f173275acbb9906905a771902bf223881ea108
gdlammel <gdlammel04@gmail.com>
2023-03-29 22:50:50 -0300

    chore: add email env config

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -4,4 +4,9 @@ export const env = {
 	port: process.env.PORT,
 	secret: process.env.JWT_SECRET as string,
 	expiresIn: process.env.JWT_EXPIRES_IN,
+	emailHost: process.env.EMAIL_HOST as string,
+	emailPort: process.env.EMAIL_PORT,
+	emailUser: process.env.EMAIL_USER as string,
+	emailPassword: process.env.EMAIL_PASSWORD as string,
+	email: process.env.EMAIL,
 };
