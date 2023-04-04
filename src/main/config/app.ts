01a9fc23a9a1b21615dcd7cdc72687ba7474199f
gdlammel <gdlammel04@gmail.com>
2023-04-03 22:19:10 -0300

    feat(main): create cors config

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,8 +1,15 @@
 import express from "express";
+import cors from 'cors'
 import { appRouter } from "./router";
 
 const app = express();
 app.use(express.json());
+
+app.use(cors({
+    credentials: true,
+    allowedHeaders: ["Authorization"],
+    origin: "*"
+}))
 
 app.use("/api", appRouter);
 
