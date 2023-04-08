9fa067b97c2d19e91d7afb85d0bb7d8aece93c1f
Guilherme Duarte <gdlammel04@gmail.com>
2023-04-07 23:45:35 -0300

    feat(adapters): create create-user gateway implementation

---
+++ src/adapters/gateways/create-user.ts
@@ -0,0 +1,20 @@
+import {IUserRepository} from "@/adapters/repositories";
+import {IHashService, IIdService} from "@/adapters/services";
+import {User} from "@/domain/entities";
+import {ICreateUserGateway} from "@/application/create-user";
+
+export class CreateUserGateway implements ICreateUserGateway{
+	constructor(private userRepository: IUserRepository, private idService: IIdService,private hashService: IHashService, ) {}
+	async findUserByEmail(email: string): Promise<User | null>{
+		return this.userRepository.findUserByEmail(email)
+	}
+	async save(user: User): Promise<boolean>{
+		return this.userRepository.save(user)
+	}
+	async hash(password: string): Promise<string>{
+		return this.hashService.hash(password)
+	}
+	generateId(): string{
+		return this.idService.generateId()
+	}
+}
