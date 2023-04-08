d1f66a86f91b90673a44e16c9e8670a0c1d1ef60
Guilherme Duarte <gdlammel04@gmail.com>
2023-04-07 23:39:36 -0300

    chore(infra): move validators to middlewares folder

---
+++ src/infra/middlewares/authentication/ensure-authentication.ts
@@ -0,0 +1,33 @@
+import { Request, Response, NextFunction } from "express";
+import { verify } from "jsonwebtoken";
+
+import { HttpPresenter } from "@/adapters/presenters";
+
+interface IPayload {
+	sub: string;
+}
+
+export class EnsureAuthenticationMiddleware {
+	constructor(private secret: string) {}
+	public verify(request: Request, response: Response, next: NextFunction) {
+		const authToken = request.headers.authorization;
+		if (!authToken) {
+			const responseData =
+				HttpPresenter.unauthorized("Invalid Token");
+			return response.status(responseData.statusCode).json(responseData);
+		}
+
+		const [_, token] = authToken.split(" ");
+		try {
+			const { sub } = verify(token, this.secret) as IPayload;
+			const body = request.body;
+			body.id = sub;
+			request.body = body;
+			return next();
+		} catch (error) {
+			const responseData =
+				HttpPresenter.unauthorized("Unauthorized");
+			return response.status(responseData.statusCode).json(responseData);
+		}
+	}
+}
---
+++ src/infra/middlewares/validators/forgot-password.ts
@@ -0,0 +1,35 @@
+import { Request, Response, NextFunction } from "express";
+import { z, ZodError } from "zod";
+
+import { HttpPresenter } from "@/adapters/presenters";
+
+const forgotPasswordSchema = z.object({
+	email: z
+		.string()
+		.email({ message: "Email invalid format" })
+		.nonempty({ message: "Email is required" }),
+});
+
+export type forgotPasswordOutputData = z.output<typeof forgotPasswordSchema>;
+
+export class validateForgotPasswordInput {
+	static validate(request: Request, response: Response, next: NextFunction) {
+		const { email } = request.body;
+		try {
+			forgotPasswordSchema.parse({ email });
+			return next();
+		} catch (error) {
+			if (error instanceof ZodError) {
+				const responseData = HttpPresenter.badRequest(
+					error.issues[0].message
+				);
+				return response
+					.status(responseData.statusCode)
+					.json(responseData);
+			}
+			const responseData =
+				HttpPresenter.internalError("Internal error");
+			return response.status(responseData.statusCode).json(responseData);
+		}
+	}
+}
