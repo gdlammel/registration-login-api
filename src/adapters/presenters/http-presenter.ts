816f133b58669666f90bca1cbd6966a24fd09dd1
Guilherme Duarte <gdlammel04@gmail.com>
2023-04-08 22:24:02 -0300

    chore: change presenter name

--- src/adapters/presenters/http-presenter.ts
+++ src/adapters/presenters/http-presenter.ts
@@ -1,36 +1,36 @@
-export interface IHttpResponse<T>{
+export interface HttpResponse<T>{
 	statusCode: number
 	data: T
 }
 export class HttpPresenter{
-	static badRequest<T>(data: T): IHttpResponse<T> {
+	static badRequest<T>(data: T): HttpResponse<T> {
 		return {
 			statusCode: 400,
 			data,
 		};
 	}
 
-	static internalError<T>(data: T): IHttpResponse<T> {
+	static internalError<T>(data: T): HttpResponse<T> {
 		return {
 			statusCode: 500,
 			data,
 		};
 	}
 
-	static created<T>(data: T): IHttpResponse<T>{
+	static created<T>(data: T): HttpResponse<T>{
 		return {
 			statusCode: 201,
 			data,
 		};
 	}
 
-	static ok<T>(data: T): IHttpResponse<T>{
+	static ok<T>(data: T): HttpResponse<T>{
 		return {
 			statusCode: 200,
 			data,
 		};
 	}
-	static unauthorized<T>(data: T): IHttpResponse<T>{
+	static unauthorized<T>(data: T): HttpResponse<T>{
 		return {
 			statusCode: 401,
 			data,
