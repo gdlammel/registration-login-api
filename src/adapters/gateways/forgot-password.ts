6d8d68cb7d58c79d122a37d68bf99e9662790302
Guilherme Duarte <gdlammel04@gmail.com>
2023-04-09 22:08:34 -0300

    feat(adapters): create forgot password gateway implementation

---
+++ src/adapters/gateways/forgot-password.ts
@@ -0,0 +1,21 @@
+import { IForgotPasswordGateway } from "@/application/forgot-password";
+import { User } from "@/domain/entities";
+import { IUserRepository } from "@/adapters/repositories";
+import { IEmailService, ITokenService } from "@/adapters/services";
+
+export class ForgotPasswordGateway implements IForgotPasswordGateway {
+	constructor(
+		private userRepository: IUserRepository,
+		private tokenService: ITokenService,
+		private emailService: IEmailService
+	) {}
+	async findUserByEmail(email: string): Promise<User | null> {
+		return this.userRepository.findUserByEmail(email);
+	}
+	generateToken(data: Object, secret?: string | undefined): string {
+		return this.tokenService.generateToken(data, secret);
+	}
+	async sendEmail(user: User, token: string): Promise<boolean> {
+		return this.emailService.sendEmail(user, token);
+	}
+}
