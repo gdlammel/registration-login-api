2d14294770a3bf2fae9deea2c7344ddbc24e7358
Guilherme Duarte <gdlammel04@gmail.com>
2023-04-09 22:14:55 -0300

    feat(adapters): create reset password gateway implementation

--- src/adapters/gateways/index.ts
+++ src/adapters/gateways/index.ts
@@ -1,2 +1,4 @@
-export * from './create-user'
-export * from "./authenticate-user"
+export * from "./create-user";
+export * from "./authenticate-user";
+export * from "./forgot-password";
+export * from "./reset-password"
---
+++ src/adapters/gateways/reset-password.ts
@@ -0,0 +1,18 @@
+import {IResetPasswordGateway} from "@/application/reset-password";
+import {User} from "@/domain/entities";
+import {IUserRepository} from "@/adapters/repositories";
+import {IHashService} from "@/adapters/services";
+
+export class ResetPasswordGateway implements IResetPasswordGateway{
+	constructor(private userRepository: IUserRepository, private hashService: IHashService) {}
+	findUserByID(id: string): Promise<User | null> {
+		return this.userRepository.findUserByID(id)
+		
+	}
+	updatePassword(user: User, newPassword: string): Promise<boolean> {
+		return this.userRepository.updatePassword(user, newPassword)
+	}
+	hash(password: string): Promise<string> {
+		return this.hashService.hash(password)
+	}
+}
