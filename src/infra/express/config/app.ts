34ed15ab832ea3497830d5a3ca03724ad675e690
Guilherme Duarte <gdlammel04@gmail.com>
2023-04-19 22:34:52 -0300

    fix(infra): fix express middleware error

--- src/infra/express/config/app.ts
+++ src/infra/express/config/app.ts
@@ -1,16 +1,19 @@
 import express from "express";
-import cors from 'cors'
+import cors from "cors";
 
-import { appRouter } from "@/infra/express/config"
+import { appRouter } from "./router";
 
 const app = express();
+
 app.use(express.json());
 
-app.use(cors({
-	credentials: true,
-	allowedHeaders: ["Authorization"],
-	origin: "*"
-}))
+app.use(
+	cors({
+		credentials: true,
+		allowedHeaders: ["Authorization"],
+		origin: "*",
+	})
+);
 
 app.use("/api", appRouter);
 
