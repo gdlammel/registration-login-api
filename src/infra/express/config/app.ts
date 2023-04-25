62593d756e3e09177b6ce61d234512cb60f8dfc1
Guilherme Duarte <gdlammel04@gmail.com>
2023-04-24 23:49:09 -0300

    refactor(infra): change files structure to use classes

--- src/infra/express/config/app.ts
+++ src/infra/express/config/app.ts
@@ -1,20 +1,69 @@
 import express from "express";
 import cors from "cors";
 
-import { appRouter } from "./router";
+import { AppRouter } from "@/infra/express/config";
+import {
+	CreateUserExpressHandlerFactory,
+	ForgotPasswordExpressHandlerFactory,
+	ResetPasswordExpressHandlerFactory,
+	AuthenticateUserExpressHandlerFactory,
+} from "@/infra/express/factories/express-handlers";
 
-const app = express();
+class ExpressApp {
+	public _app = express();
+	private appRouter: AppRouter;
 
-app.use(express.json());
+	constructor() {
+		this.appRouter = this.createAppRouter();
+		this.configureMiddlewares();
+		this.configureRoutes();
+	}
 
-app.use(
-	cors({
-		credentials: true,
-		allowedHeaders: ["Authorization"],
-		origin: "*",
-	})
-);
+	private initializeFactories() {
+		return {
+			createUserHandlerFactory: new CreateUserExpressHandlerFactory(),
+			forgotPasswordHandlerFactory:
+				new ForgotPasswordExpressHandlerFactory(),
+			resetPasswordHandlerFactory:
+				new ResetPasswordExpressHandlerFactory(),
+			authenticateUserHandlerFactory:
+				new AuthenticateUserExpressHandlerFactory(),
+		};
+	}
 
-app.use("/api", appRouter);
+	private createAppRouter() {
+		const {
+			createUserHandlerFactory,
+			forgotPasswordHandlerFactory,
+			resetPasswordHandlerFactory,
+			authenticateUserHandlerFactory,
+		} = this.initializeFactories();
 
-export { app };
+		return new AppRouter(
+			createUserHandlerFactory,
+			forgotPasswordHandlerFactory,
+			resetPasswordHandlerFactory,
+			authenticateUserHandlerFactory
+		);
+	}
+	private configureMiddlewares() {
+		this._app.use(express.json());
+		this._app.use(
+			cors({
+				credentials: true,
+				allowedHeaders: ["Authorization"],
+				origin: "*",
+			})
+		);
+	}
+
+	private configureRoutes() {
+		this._app.use("/api", this.appRouter.configureRoutes());
+	}
+
+	public get app() {
+		return this._app;
+	}
+}
+
+export { ExpressApp };
--- src/infra/express/config/router.ts
+++ src/infra/express/config/router.ts
@@ -1,10 +1,40 @@
 import { Router } from "express";
 
-import { sessionRoutes, userRoutes } from "@/infra/express/routes";
+import { UserRouter } from "@/infra/express/routes";
+import { SessionRouter } from "@/infra/express/routes";
 
-const appRouter = Router();
+import {
+	AuthenticateUserExpressHandlerFactory,
+	CreateUserExpressHandlerFactory,
+	ForgotPasswordExpressHandlerFactory,
+	ResetPasswordExpressHandlerFactory,
+} from "@/infra/express/factories/express-handlers";
 
-appRouter.use("/users", userRoutes);
-appRouter.use("/session", sessionRoutes);
+class AppRouter {
+	private router: Router = Router();
 
-export { appRouter };
+	constructor(
+		private createUserHandlerFactory: CreateUserExpressHandlerFactory,
+		private forgotPasswordHandlerFactory: ForgotPasswordExpressHandlerFactory,
+		private resetPasswordHandlerFactory: ResetPasswordExpressHandlerFactory,
+		private authenticateUserHandlerFactory: AuthenticateUserExpressHandlerFactory
+	) {}
+
+	public configureRoutes(): Router {
+		const userRouter = new UserRouter(
+			this.createUserHandlerFactory,
+			this.forgotPasswordHandlerFactory,
+			this.resetPasswordHandlerFactory
+		);
+
+		const sessionRouter = new SessionRouter(
+			this.authenticateUserHandlerFactory
+		);
+
+		this.router.use("/users", userRouter.configureRoutes());
+		this.router.use("/session", sessionRouter.configureRoutes());
+		return this.router;
+	}
+}
+
+export { AppRouter };
