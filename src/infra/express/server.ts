29f0469ba556f584ba40a7d7d0cb8a704e931f2c
Guilherme Duarte <gdlammel04@gmail.com>
2023-04-24 23:50:27 -0300

    refactor(infra): change server file to instantiate new Express App class

--- src/infra/express/server.ts
+++ src/infra/express/server.ts
@@ -1,6 +1,8 @@
-import { env } from "@/infra/env"
-import { app } from "@/infra/express/config";
+import { env } from "@/infra/env";
+import { ExpressApp } from "@/infra/express/config";
 
-app.listen(env.port, () => {
+const expressApp = new ExpressApp();
+
+expressApp.app.listen(env.port, () => {
 	console.log("Server is running");
 });
