64a45e7f227341e3f4ed9ede38cf384e78f020bb
Guilherme Duarte <gdlammel04@gmail.com>
2023-04-28 02:12:39 -0300

    chore: add new env config to env object

--- src/infra/env/index.ts
+++ src/infra/env/index.ts
@@ -3,12 +3,15 @@ dotenv.config();
 export const env = {
 	port: process.env.PORT,
 	secret: process.env.JWT_SECRET as string,
-	expiresIn: process.env.JWT_EXPIRES_IN,
+	expiresIn: process.env.JWT_EXPIRES_IN as string,
 	emailHost: process.env.EMAIL_HOST as string,
 	emailPort: process.env.EMAIL_PORT,
 	emailUser: process.env.EMAIL_USER as string,
 	emailPassword: process.env.EMAIL_PASSWORD as string,
 	email: process.env.EMAIL,
 	forgotPasswordSecret: process.env.JWT_FORGOT_PASSWORD_SECRET as string,
-	forgotPasswordExpiresIn: process.env.JWT_FORGOT_PASSWORD_EXPIRES_IN as string,
+	forgotPasswordExpiresIn: process.env
+		.JWT_FORGOT_PASSWORD_EXPIRES_IN as string,
+	jwt2FaSecret: process.env.JWT_2FA_SECRET as string,
+	jwt2FaExpiresIn: process.env.JWT_2FA_EXPIRES_IN as string,
 };
