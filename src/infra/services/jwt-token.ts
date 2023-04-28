7bfc0d63721d3b7cf88d3a8e6f9c49fb0d002037
Guilherme Duarte <gdlammel04@gmail.com>
2023-04-28 02:59:09 -0300

    feat(infra): add parameters to token service implementation

--- src/infra/services/jwt-token.ts
+++ src/infra/services/jwt-token.ts
@@ -5,10 +5,14 @@ import { env } from "@/infra/env";
 import { User } from "@/domain/entities";
 
 export class JwtTokenService implements ITokenService {
-	generateToken(data: User, secret: string = env.secret): string {
+	generateToken(
+		data: User,
+		secret: string = env.secret,
+		expiresIn: string = env.expiresIn
+	): string {
 		const token = sign({ id: data.id }, secret, {
 			subject: data.id,
-			expiresIn: env.expiresIn,
+			expiresIn: expiresIn,
 		});
 
 		return token;
