00ffcd2d958b6df598e7f565fff652698c4fb4fc
Guilherme Duarte <gdlammel04@gmail.com>
2023-04-28 02:24:34 -0300

    add totp seed return

--- src/adapters/controllers/create-user.ts
+++ src/adapters/controllers/create-user.ts
@@ -1,20 +1,26 @@
 import { CreateUserInteractor } from "@/application/create-user";
-import {Controller} from "@/adapters/controllers/common"
+import { Controller } from "@/adapters/controllers/common";
 import { EmailAlreadyRegisteredError } from "@/application/create-user/errors";
 import { InternalError } from "@/application/common/errors";
 import { UserDomainError } from "@/domain/entities";
 import { HttpPresenter, HttpResponse } from "@/adapters/presenters";
 
 export interface CreateUserRequestDTO {
-		name: string;
-		password: string;
-		email: string;
-		phoneNumber: number
+	name: string;
+	password: string;
+	email: string;
+	phoneNumber: number;
 }
-export class CreateUserController implements Controller<CreateUserRequestDTO, HttpResponse<string>> {
+export class CreateUserController
+	implements Controller<CreateUserRequestDTO, HttpResponse<string>>
+{
 	constructor(private createUserInteractor: CreateUserInteractor) {}
-	async handle({name, password, email, phoneNumber}: CreateUserRequestDTO): Promise<HttpResponse<string>>{
-
+	async handle({
+		name,
+		password,
+		email,
+		phoneNumber,
+	}: CreateUserRequestDTO): Promise<HttpResponse<string>> {
 		const result = await this.createUserInteractor.execute({
 			name,
 			password,
@@ -25,11 +31,11 @@ export class CreateUserController implements Controller<CreateUserRequestDTO, Ht
 		if (
 			result instanceof EmailAlreadyRegisteredError ||
 			result instanceof UserDomainError
-			) {
-			return HttpPresenter.badRequest("Error creating new user")
+		) {
+			return HttpPresenter.badRequest("Error creating new user");
 		} else if (result instanceof InternalError) {
 			return HttpPresenter.internalError("Internal error");
-		} 
-		return HttpPresenter.created("User created with success!");
+		}
+		return HttpPresenter.created(result);
 	}
 }
