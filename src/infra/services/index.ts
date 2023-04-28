8f06ea0346e0f46020f681d5a8ad4c93aeca81da
Guilherme Duarte <gdlammel04@gmail.com>
2023-04-28 02:59:56 -0300

    feat(infra): add totp export

--- src/infra/services/index.ts
+++ src/infra/services/index.ts
@@ -1,4 +1,5 @@
 export * from "./uuid-id";
 export * from "./bcrypt-hash";
 export * from "./nodemailer-email";
-export * from "./jwt-token"
+export * from "./jwt-token";
+export * from "./otplib-totp";
