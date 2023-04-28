88e985d1424d3817372dc692a5184d1b3a604ab7
Guilherme Duarte <gdlammel04@gmail.com>
2023-04-28 02:17:15 -0300

    feat(application): add generate totp seed method to create user gateway

--- src/application/create-user/create-user-gateway.ts
+++ src/application/create-user/create-user-gateway.ts
@@ -1,8 +1,9 @@
-import {User} from "@/domain/entities";
+import { User } from "@/domain/entities";
 
-export interface ICreateUserGateway{
-	findUserByEmail(email: string): Promise<User | null>
-	save(user: User): Promise<boolean>
-	hash(password: string): Promise<string>
-	generateId(): string
+export interface ICreateUserGateway {
+	findUserByEmail(email: string): Promise<User | null>;
+	save(user: User): Promise<boolean>;
+	hash(password: string): Promise<string>;
+	generateId(): string;
+	generateTotpSeed(name: string, secret: string): string;
 }
